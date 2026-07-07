0be03ed56a233c579af048a3f34bc08b504ed772
z3c0d3ru5h <pierrick.phieu@gmail.com>
2026-07-07 05:28:30 +0200

    Add Link to /about route and style

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";   
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About Screen 
+      </Link>
     </View>
   );
 }
@@ -16,6 +20,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   text: {
+    color: "#fff",
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: "underline",
     color: "#fff",
   },
 });
