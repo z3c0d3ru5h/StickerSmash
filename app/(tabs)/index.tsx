1c0c82e4a0af4fdc8f61d18a355ae674375ba8c8
z3c0d3ru5h <pierrick.phieu@gmail.com>
2026-07-08 22:37:01 +0200

    Create buttons using Pressable

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import { View, StyleSheet } from "react-native";
 
+import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
@@ -9,6 +10,10 @@ export default function Index() {
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
       </View>
     </View>
   );
@@ -22,5 +27,9 @@ const styles = StyleSheet.create({
   },
   imageContainer: {
     flex: 1,
+  },
+  footerContainer: {
+    flex: 1 /3,
+    alignItems: "center",
   },
 });
---
+++ components/Button.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet, View, Pressable, Text } from "react-native";
+
+type Props = {
+  label: string;
+};
+
+export default function Button({ label }: Props) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert("You pressed a bouton")}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+   button: {
+    borderRadius: 10,
+    width: "100%", 
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  buttonLabel: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
